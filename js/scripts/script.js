095ad76d4ca40a210a710644d509e65514c44b47
kumaxim94 <kumaxim94@gmail.com>
2017-11-29 20:38:59 +0200

    pushing!

--- js/scripts/script.js
+++ js/scripts/script.js
@@ -2,43 +2,102 @@ var respImg = document.getElementById('respImg');
 var respBtn = document.getElementById('respBtn');
 
 function resp() {
-  if (respImg.hasAttribute('style')) {
-    respImg.removeAttribute('style');
-    respBtn.innerHTML = 'Add responsive';
-    return;
-  }
-  respImg.setAttribute('style', 'max-width:100%;height:auto;');
-  respBtn.innerHTML = 'Remove responsive';
+    if (respImg.hasAttribute('style')) {
+        respImg.removeAttribute('style');
+        respBtn.innerHTML = 'Add responsive';
+        return;
+    }
+    respImg.setAttribute('style', 'max-width:100%;height:auto;');
+    respBtn.innerHTML = 'Remove responsive';
 }
 
 
 (function ($) {
-  'use strict';
-  var brepet = $('#brepet');
-  var battach = $('#battach');
-  var bclip = $('#bclip');
-  var borig = $('#borig');
-  var bsize = $('#bsize');
-  var over = $('#over');
-  var brepetR = $('.radioRepeat');
-  var brepetRO = $('.brepetRO');
+    'use strict';
+    var brepet = $('#brepet');
+    var battach = $('#battach');
+    var bclip = $('#bclip');
+    var borig = $('#borig');
+    var bsize = $('#bsize');
+    var over = $('#over');
+    var brepetR = $('.radioRepeat');
+    var brepetRO = $('.brepetRO');
 
-  $(document).ready(function () {
-    brepetR.on('click', function (e) {
-      var target = e.target;
-      var datav = target.getAttribute('data-content');
-      brepet.css('background-repeat', datav);
-      battach.css('background-attachment', datav);
-      bsize.css('background-size', datav);
-      bclip.css('background-clip', datav);
-      over.css('overflow', datav);
+
+    var flexToggler = $('.flex-toggler');
+    var toggleGrower1 = $('.flex-togglerGrow-1');
+    var toggleGrower2 = $('.flex-togglerGrow-2');
+    var toggleGrower3 = $('.flex-togglerGrow-3');
+
+
+    var justifying = $('#justifying');
+    var itemAlign = $('#item-align');
+    var flexDir = $('#flex-dir');
+    var flexWrap = $('#flex-wrap');
+    var contAlign = $('#cont-align');
+    var flexGrow1 = $('#flex-grow-1');
+    var flexGrow2 = $('#flex-grow-2');
+    var flexGrow3 = $('#flex-grow-3');
+    var flexShrink1 = $('#flex-shrink-1');
+    var flexShrink2 = $('#flex-shrink-2');
+    var flexShrink3 = $('#flex-shrink-3');
+
+
+    $(document).ready(function () {
+        brepetR.on('click', function (e) {
+            var target = e.target;
+            var datav = target.getAttribute('data-content');
+            brepet.css('background-repeat', datav);
+            battach.css('background-attachment', datav);
+            bsize.css('background-size', datav);
+            bclip.css('background-clip', datav);
+            over.css('overflow', datav);
+        });
+
+        brepetRO.on('click', function (e) {
+            var target = e.target;
+            var datav = target.getAttribute('data-content');
+            borig.css('background-origin', datav);
+        });
+
+        flexToggler.on('click', function (e) {
+            var target = e.target;
+            var flexAlign = target.getAttribute('data-value');
+            justifying.css('justify-content', flexAlign);
+            itemAlign.css('align-items', flexAlign);
+            flexDir.css('flex-direction', flexAlign);
+            flexWrap.css('flex-wrap', flexAlign);
+            contAlign.css('align-content', flexAlign);
+
+        });
+
+        toggleGrower1.on('click', function (e) {
+            var target = e.target;
+            var flexAlign = target.getAttribute('data-value');
+            flexGrow1.css('flex-grow', flexAlign);
+            flexShrink1.css('flex-shrink', flexAlign);
+
+        });
+
+        toggleGrower2.on('click', function (e) {
+            var target = e.target;
+            var flexAlign = target.getAttribute('data-value');
+            flexGrow2.css('flex-grow', flexAlign);
+            flexShrink2.css('flex-shrink', flexAlign);
+
+        });
+
+        toggleGrower3.on('click', function (e) {
+            var target = e.target;
+            var flexAlign = target.getAttribute('data-value');
+            flexGrow3.css('flex-grow', flexAlign);
+            flexShrink3.css('flex-shrink', flexAlign);
+
+        });
+
     });
 
-    brepetRO.on('click', function (e) {
-      var target = e.target;
-      var datav = target.getAttribute('data-content');
-      borig.css('background-origin', datav);
-    })
-  });
+
 })(jQuery);
+
 
